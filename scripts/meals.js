18bd41788866e5a1dc43c08771350f40fdf057a1
Th3RedMan <parkerphillips2003@gmail.com>
2023-01-28 20:17:37 -0800

    Initial Commit

---
+++ scripts/meals.js
@@ -0,0 +1,38 @@
+const arrowDropFirst = document.querySelector(".first-arw")
+const foodItemFirst = document.querySelectorAll(".item-first")
+const arrowDropSec = document.querySelector(".sec-arw")
+const foodItemSec = document.querySelectorAll(".item-sec")
+foodItemFirstActive = true
+foodItemSecActive = true
+
+arrowDropFirst.addEventListener("click", () => {
+    if (foodItemFirstActive) {
+        foodItemFirst.forEach(item => {
+            item.style.display = "none"
+            arrowDropFirst.innerHTML = "&and;"
+            foodItemFirstActive = false
+        })
+    } else {
+        foodItemFirst.forEach(item => {
+            item.style.display = "flex"
+            arrowDropFirst.innerHTML = "&or;"
+            foodItemFirstActive = true
+        })
+    }
+})
+
+arrowDropSec.addEventListener("click", () => {
+    if (foodItemSecActive) {
+        foodItemSec.forEach(item => {
+            item.style.display = "none"
+            arrowDropSec.innerHTML = "&and;"
+            foodItemSecActive = false
+        })
+    } else {
+        foodItemSec.forEach(item => {
+            item.style.display = "flex"
+            arrowDropSec.innerHTML = "&or;"
+            foodItemSecActive = true
+        })
+    }
+})
