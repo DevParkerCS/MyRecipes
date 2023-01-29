32620aa120599d10a56b205b1e74c618b43fcb73
Th3RedMan <parkerphillips2003@gmail.com>
2023-01-29 13:38:57 -0800

    fixed scaling issues

--- scripts/meals.js
+++ scripts/meals.js
@@ -2,19 +2,22 @@ const arrowDropFirst = document.querySelector(".first-arw")
 const foodItemFirst = document.querySelectorAll(".item-first")
 const arrowDropSec = document.querySelector(".sec-arw")
 const foodItemSec = document.querySelectorAll(".item-sec")
+const arrowDropThird = document.querySelector(".third-arw")
+const foodItemThird = document.querySelectorAll(".item-third")
 foodItemFirstActive = true
 foodItemSecActive = true
+foodItemThirdActive = true
 
 arrowDropFirst.addEventListener("click", () => {
     if (foodItemFirstActive) {
         foodItemFirst.forEach(item => {
-            item.style.display = "none"
+            item.classList.add("inactive")
             arrowDropFirst.innerHTML = "&#62;"
             foodItemFirstActive = false
         })
     } else {
         foodItemFirst.forEach(item => {
-            item.style.display = "flex"
+            item.classList.remove("inactive")
             arrowDropFirst.innerHTML = "&or;"
             foodItemFirstActive = true
         })
@@ -24,15 +27,31 @@ arrowDropFirst.addEventListener("click", () => {
 arrowDropSec.addEventListener("click", () => {
     if (foodItemSecActive) {
         foodItemSec.forEach(item => {
-            item.style.display = "none"
+            item.classList.add("inactive")
             arrowDropSec.innerHTML = "&#62;"
             foodItemSecActive = false
         })
     } else {
         foodItemSec.forEach(item => {
-            item.style.display = "flex"
+            item.classList.remove("inactive")
             arrowDropSec.innerHTML = "&or;"
             foodItemSecActive = true
+        })
+    }
+})
+
+arrowDropThird.addEventListener("click", () => {
+    if (foodItemThirdActive) {
+        foodItemThird.forEach(item => {
+            item.classList.add("inactive")
+            arrowDropThird.innerHTML = "&#62;"
+            foodItemThirdActive = false
+        })
+    } else {
+        foodItemThird.forEach(item => {
+            item.classList.remove("inactive")
+            arrowDropThird.innerHTML = "&or;"
+            foodItemThirdActive = true
         })
     }
 })
