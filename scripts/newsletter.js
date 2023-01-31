c6ff656105722c87050251700314dac419a7caf0
Th3RedMan <parkerphillips2003@gmail.com>
2023-01-30 16:19:29 -0800

    Added newsletter popup functionality

--- scripts/newsletter.js
+++ scripts/newsletter.js
@@ -1,15 +1,19 @@
 const newsletterBtn = document.querySelector(".newsletter-btn")
 const popup = document.querySelector(".popup")
 const popupClose = document.querySelector(".popup-exit")
+const pageWrapper = document.querySelector(".page-wrapper")
 let poopupActive = false
 
 newsletterBtn.addEventListener("click", () => {
     if (poopupActive == false) {
         popup.classList.add("popup-active")
-        document.body.classList.add("inactive-body")
+        pageWrapper.classList.add("darken-page")
+        document.body.classList.add("body-inactive")
     }
 })
 
 popupClose.addEventListener("click", () => {
     popup.classList.remove("popup-active")
+    document.body.classList.remove("body-inactive")
+    pageWrapper.classList.remove("darken-page")
 })
