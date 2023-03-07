23fa03877897f6b59f3980cb509ecb98d9846ce6
Th3RedMan <parkerphillips2003@gmail.com>
2023-03-06 16:06:40 -0800

    davids commit

--- scripts/nav.js
+++ scripts/nav.js
@@ -1,18 +1,15 @@
 const hamburgerBtn = document.querySelector(".hamburger")
-const navWrapper = document.querySelector(".nav-wrapper")
 const navAWrapper = document.querySelector(".nav-a-wrapper")
 
 let navShown = false
 
 hamburgerBtn.addEventListener("click", () => {
     if (!navShown) {
-        navAWrapper.classList.add("nav-shown")
-        navWrapper.classList.add("nav-shown")
+        navAWrapper.style.right = 0
         navShown = true
     }
     else {
-        navAWrapper.classList.remove("nav-shown")
-        navWrapper.classList.remove("nav-shown")
+        navAWrapper.style.right = `100%`
         navShown = false
     }
 })
--- scripts/news.js
+++ scripts/news.js
@@ -1,20 +1,24 @@
 const loadMoreButton = document.querySelector(".load-more")
-const loadedNews = document.querySelector(".news-hidden")
+const loadedNews = document.querySelectorAll(".news-hidden")
 let newsVisible = false
 const showLessText = '<div class="load-more-txt">Show Less Results</div><div class="load-more-btn">&and;</div>'
 const showMoreText = '<div class="load-more-txt">Show More Results</div><div class="load-more-btn">&or;</div>'
 
 
 loadMoreButton.addEventListener("click", () => {
-    if (!newsVisible) {
-        loadedNews.classList.remove("news-hidden")
-        loadMoreButton.innerHTML = showLessText
+    loadedNews.forEach(news => {
+        if (!newsVisible) {
+            news.classList.remove("news-hidden")
+            loadMoreButton.innerHTML = showLessText
+        }
+        else {
+            news.classList.add("news-hidden")
+            loadMoreButton.innerHTML = showMoreText
+        }
+    })
 
+    if (!newsVisible) {
         newsVisible = true
     }
-    else {
-        loadedNews.classList.add("news-hidden")
-        loadMoreButton.innerHTML = showMoreText
-        newsVisible = false
-    }
+    else { newsVisible = false }
 })
